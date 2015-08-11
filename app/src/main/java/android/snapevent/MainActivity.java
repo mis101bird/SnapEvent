766d38887adecbee8c018bfdedc13ad5a6f8655f
Hsuan-ju lin <mis101bird@gmail.com>
2015-08-11 21:09:36 +0800

    make toolbar

--- app/src/main/java/android/snapevent/MainActivity.java
+++ app/src/main/java/android/snapevent/MainActivity.java
@@ -1,10 +1,47 @@
 package android.snapevent;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+import android.view.View;
+import android.widget.Adapter;
 
 /**
  * Created by hsuan-ju on 2015/8/10.
  */
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+
+
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowTitleEnabled(false);
+        //ab.setDisplayHomeAsUpEnabled(true);
+
+      ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+
+      //  adapter.addFragment(new CheeseListFragment(), "Category 1");
+
+      //  viewPager.setAdapter(adapter);
+    }
 }
